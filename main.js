3b3c12c5ddf366a20321fe378e78b605609f0863
nicopono <47738551+nicopono@users.noreply.github.com>
2019-03-31 22:12:25 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+function menuBurger () {
+
+ var menuResponsive = document.getElementById ("menuResponsive");
+
+//  menuResponsive.style.visibility= "hidden";
+
+ if (menuResponsive.style.visibility== "hidden")  {
+
+     menuResponsive.style.visibility= "initial";
+ }
+ else {    
+     menuResponsive.style.visibility= "hidden";
+}
+}
